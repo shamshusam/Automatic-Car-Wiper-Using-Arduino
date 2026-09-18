15f85e1cc8122a5686770472e78583010cf6f206
Shamshuddin <160005353+shamshusam@users.noreply.github.com>
2026-09-18 14:09:51 +0530

    Update automatic-car-wiper.ino

--- documentation/code/automatic-car-wiper.ino
+++ documentation/code/automatic-car-wiper.ino
@@ -1,1 +1,45 @@
+#include <Servo.h>
 
+Servo wiperServo;
+
+const int rainSensorPin = A0;
+const int servoPin = 9;
+
+int rainValue;
+int rainThreshold = 500;
+
+void setup() {
+  Serial.begin(9600);
+
+  wiperServo.attach(servoPin);
+  wiperServo.write(0);
+}
+
+void loop() {
+
+  rainValue = analogRead(rainSensorPin);
+
+  Serial.print("Rain Sensor Value: ");
+  Serial.println(rainValue);
+
+  if (rainValue < rainThreshold) {
+
+    // Rain detected
+    for (int angle = 0; angle <= 120; angle++) {
+      wiperServo.write(angle);
+      delay(10);
+    }
+
+    for (int angle = 120; angle >= 0; angle--) {
+      wiperServo.write(angle);
+      delay(10);
+    }
+
+  } else {
+
+    // No rain
+    wiperServo.write(0);
+  }
+
+  delay(100);
+}
